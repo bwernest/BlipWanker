24d4b65fe4e1982473e03833ced1e80a04c2d42c
bwernest <ernest.laprelleboucher@gmail.com>
2025-08-02 12:04:29 +0200

    Introduces analysor

---
+++ engine/analyse/analysor.py
@@ -0,0 +1,18 @@
+"""___Modules_______________________________________________________________"""
+
+# BlipWanker
+from toolbox import *
+
+# Python
+from copy import deepcopy
+from matplotlib import pyplot as plt
+import numpy as np
+from tqdm import tqdm
+from typing import Dict, List, Tuple
+
+"""___Classes_______________________________________________________________"""
+
+class Analysor():
+
+    def __init__(self, grid: Dict = {}):
+        pass
